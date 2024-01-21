df8a0bae11537865f56ffffda386e77bcdc2d58f
MatRos-sf <mateuszrosenkranz@gmail.com>
2024-01-21 11:15:53 +0100

    add new views

--- src/epuls/settings.py
+++ src/epuls/settings.py
@@ -36,12 +36,14 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles'
 ]
 
 INSTALLED_EXTENSIONS = [
     'account'
 ]
+
+INSTALLED_APPS += INSTALLED_EXTENSIONS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
