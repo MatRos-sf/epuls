139a479889628d9da21590d9352e09e00a54b5d1
MatRos-sf <mateuszrosenkranz@gmail.com>
2024-01-22 22:42:00 +0100

    update ProfileView

--- src/account/views.py
+++ src/account/views.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import UserSignupForm
 from django.views.generic import DetailView
 
-from .models import Profile
+from .models import Profile, Visitor
+from django.contrib.auth.models import User
+
 
 def signup(request) -> HttpResponse:
     form = UserSignupForm()
@@ -20,8 +22,11 @@ class ProfileView(LoginRequiredMixin, DetailView):
     model = Profile
     template_name = 'account/profile.html'
 
-
     def get_object(self, queryset=None):
         username = self.kwargs.get('slag')
-        if username != self.request.user.username:
-            pass
+        user_instance = get_object_or_404(User, username=username)
+        if user_instance != self.request.user.username:
+            # user is Visitor
+            Visitor.objects.create(visitor=self.request.user, receiver=user_instance)
+        return user_instance
+
