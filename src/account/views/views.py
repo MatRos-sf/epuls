21070cfe58f20deb46e7addf98c496b6dafd87b4
MatRos-sf <mateuszrosenkranz@gmail.com>
2024-02-10 18:33:53 +0100

    split view file

--- src/account/views/views.py
+++ src/account/views/views.py
@@ -4,22 +4,10 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.models import User
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render, reverse
-from django.views.generic import (
-    CreateView,
-    DeleteView,
-    DetailView,
-    ListView,
-    UpdateView,
-)
+from django.views.generic import DetailView, ListView, UpdateView
 
-from .forms import (
-    AboutUserForm,
-    DiaryForm,
-    GuestbookUserForm,
-    ProfileForm,
-    UserSignupForm,
-)
-from .models import AboutUser, Diary, Guestbook, Profile, Visitor
+from account.forms import AboutUserForm, GuestbookUserForm, ProfileForm, UserSignupForm
+from account.models import AboutUser, Guestbook, Profile, Visitor
 
 
 def signup(request) -> HttpResponse:
@@ -123,90 +111,6 @@ class GuestbookView(LoginRequiredMixin, ListView):
             instance.save()
 
         return self.get(request, *args, **kwargs)
-
-
-# CRUD
-class DiaryCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
-    template_name = "account/diary/create.html"
-    model = Diary
-    form_class = DiaryForm
-    extra_context = {"action": "Edit"}
-
-    def form_valid(self, form):
-        instance = form.save(commit=False)
-        instance.author = self.request.user
-        instance.save()
-        return super().form_valid(form)
-
-    def test_func(self):
-        username = self.kwargs.get("username")
-        return self.request.user.username == username
-
-
-class DiaryDetailView(LoginRequiredMixin, DetailView):
-    template_name = "account/diary/detail.html"
-    model = Diary
-
-    def get_object(self, queryset=None):
-        username = self.kwargs.get("username")
-        pk = int(self.kwargs.get("pk"))
-
-        if username == self.request.user.username:
-            return Diary.objects.get(author__username=username, pk=pk)
-        else:
-            instance = Diary.objects.get(author__username=username, pk=pk)
-            if instance.is_hide:
-                return None
-            return instance
-
-
-class DiaryUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
-    template_name = "account/diary/create.html"
-    model = Diary
-    form_class = DiaryForm
-    extra_context = {"action": "Update"}
-
-    def test_func(self):
-        username = self.kwargs.get("username")
-        return self.request.user.username == username
-
-    def get_object(self, queryset=None):
-        return get_object_or_404(
-            Diary,
-            author__username=self.kwargs.get("username"),
-            pk=int(self.kwargs.get("pk")),
-        )
-
-
-class DiaryDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
-    model = Diary
-    template_name = "account/diary/confirm_delete.html"
-
-    def get_success_url(self):
-        username = self.request.user.username
-        return reverse("account:diary", kwargs={"username": username})
-
-    def test_func(self):
-        username = self.kwargs.get("username")
-        return self.request.user.username == username
-
-    def get_object(self, queryset=None):
-        return get_object_or_404(
-            Diary,
-            author__username=self.kwargs.get("username"),
-            pk=int(self.kwargs.get("pk")),
-        )
-
-
-class DiaryListView(LoginRequiredMixin, ListView):
-    template_name = "account/diary/list.html"
-
-    def get_queryset(self):
-        username = self.kwargs.get("username")
-        user = get_object_or_404(User, username=username)
-        if user == self.request.user:
-            return Diary.objects.filter(author=user)
-        return Diary.objects.filter(author=user, is_hide=False)
 
 
 class FriendsListView(LoginRequiredMixin, ListView):
