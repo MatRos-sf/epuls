56355e7c5fc270aff6ff8b34d9c3f76b5cdcb1d1
MatRos-sf <mateuszrosenkranz@gmail.com>
2024-02-15 21:59:28 +0100

    implemented three models: Picture, Gallery and ProfilePicture

---
+++ src/photo/models.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class ProfilePicture(models.Model):
+    picture = models.ImageField(upload_to="profile_picture", default="default.jpg")
+
+    is_accepted = models.BooleanField(default=False)
+    date_accepted = models.DateTimeField(blank=True, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+
+class Gallery(models.Model):
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+
+class Picture(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    picture = models.ImageField(upload_to="picture")
+    gallery = models.ForeignKey(Gallery, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    # likes
