b8796a92394444ce52a9b0ce9fd1dc85254d5f9b
MatRos-sf <mateuszrosenkranz@gmail.com>
2024-02-16 12:27:55 +0100

    add static url

--- src/photo/models.py
+++ src/photo/models.py
@@ -1,18 +1,21 @@
-from django.contrib.auth.models import User
 from django.db import models
 
 
 class ProfilePicture(models.Model):
-    picture = models.ImageField(upload_to="profile_picture", default="default.jpg")
+    picture = models.ImageField(
+        upload_to="profile_picture", default="default_photo_picture.jpg"
+    )
 
     is_accepted = models.BooleanField(default=False)
+    is_rejected = models.BooleanField(default=False)
+
     date_accepted = models.DateTimeField(blank=True, null=True)
     date_created = models.DateTimeField(auto_now_add=True)
 
 
 class Gallery(models.Model):
     name = models.CharField(max_length=100)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile = models.ForeignKey("account.Profile", on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
 
 
@@ -21,7 +24,7 @@ class Picture(models.Model):
     description = models.TextField(max_length=250)
     picture = models.ImageField(upload_to="picture")
     gallery = models.ForeignKey(Gallery, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile = models.ForeignKey("account.Profile", on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
 
     # likes
