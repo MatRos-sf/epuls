fb6e9a4120a08cd15fd1edc903bd8f9381a44e1e
MatRos-sf <mateuszrosenkranz@gmail.com>
2024-02-17 12:24:23 +0100

    add new field to Profile -voivodeship

--- src/epuls/settings.py
+++ src/epuls/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "localflavor",
     "crispy_forms",
     "crispy_bootstrap4",
 ]
