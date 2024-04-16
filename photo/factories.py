a60e14d47b4db05e2310876191f9a5929f49e5d6
MatRos-sf <mateuszrosenkranz@gmail.com>
2024-04-16 12:47:45 +0200

    create factory for Picture

--- photo/factories.py
+++ photo/factories.py
@@ -1,9 +1,10 @@
-from factory import Faker, Sequence, SubFactory
-from factory.django import DjangoModelFactory
+from django.core.files.base import ContentFile
+from factory import Faker, LazyAttribute, Sequence, SubFactory
+from factory.django import DjangoModelFactory, ImageField
 
 from account.factories import ProfileFactory, UserFactory
 
-from .models import Gallery
+from .models import Gallery, Picture
 
 
 class GalleryFactory(DjangoModelFactory):
@@ -13,3 +14,15 @@ class GalleryFactory(DjangoModelFactory):
     name = Sequence(lambda n: f"gallery_{n}")
     description = Faker("sentence")
     profile = SubFactory(ProfileFactory)
+
+
+class PictureFactory(DjangoModelFactory):
+    class Meta:
+        model = Picture
+
+    title = Sequence(lambda n: f"photo_{n}")
+    description = Faker("sentence")
+    picture = ImageField(filename="test_picture.jpg")
+    gallery = SubFactory(GalleryFactory)
+    profile = SubFactory(ProfileFactory)
+    presentation_tag = Sequence(lambda n: f"tag_{n}")
