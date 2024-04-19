4c16d3dad9bdd8d7386e264b041bd89b34b96b88
MatRos-sf <mateuszrosenkranz@gmail.com>
2024-04-19 13:44:51 +0200

    add debug tools

--- epuls/urls.py
+++ epuls/urls.py
@@ -5,10 +5,14 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    # conflict with profile
+    path("silk/", include("silk.urls", namespace="silk")),
     path("", include("account.urls")),
     path("photo/", include("photo.urls")),
+    path("__debug__/", include("debug_toolbar.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 # if settings.DEBUG:
-#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#     urlpatterns += [path('silk/', include('silk.urls', namespace='silk'))]
