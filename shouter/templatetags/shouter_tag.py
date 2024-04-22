570e8a7f59fbb8ad54a0e358a6ae8cb80016acc3
MatRos-sf <mateuszrosenkranz@gmail.com>
2024-04-22 21:20:01 +0200

    create shouter tag

--- shouter/templatetags/shouter_tag.py
+++ shouter/templatetags/shouter_tag.py
@@ -1,46 +1,62 @@
+from typing import Any
+
 from django import template
 from django.contrib.auth.models import User
 from django.core.cache import cache
+from django.db.models import QuerySet
+from django.http import HttpRequest
 from django.shortcuts import reverse
 from django.utils.html import strip_tags
 from django.utils.safestring import mark_safe
-from faker import Faker
 
 from shouter.models import Shouter
 
 register = template.Library()
 
 
-def make_user_tag(user: User) -> str:
-    username = user.username
-    url = reverse("account:profile", kwargs={"username": username})
+def get_user_profile_url(username: str) -> str:
+    """
+    Generate URL for user profile page.
+    """
+    return reverse("account:profile", kwargs={"username": username})
 
-    return '<a href="{url}">{username}</a>'.format(url=url, username=username)
+
+def make_user_tag(user: User) -> str:
+    """
+    Generate HTML tag for user with link to their profile.
+    """
+    username = user.username
+    url = get_user_profile_url(username)
+
+    return f'<a href="{url}" class="fs-5 fw-bold link-offset-2 link-underline link-underline-opacity-0 link-secondary">{username}</a>'
+
+
+def get_shouter_queryset() -> QuerySet:
+    """Get queryset for Shouter object"""
+    return Shouter.objects.order_by("?")[:10]
 
 
 @register.simple_tag(takes_context=True)
-def shouter(context):
+def shouter(context) -> Any:
+    """Display a list of random shouters."""
+
     request = context["request"]
+    cache_key = f"cached_informations_{request.session.session_key}"
+    cached_information = cache.get(cache_key)
 
-    if not request.user.is_authenticated:
-        qs = Shouter.objects.order_by("?")[:10]
+    if cached_information:
+        return mark_safe(cached_information)  # nosec
 
-    else:
-        cache_key = f"cached_informations_{request.session.session_key}"
-        cached_information = cache.get(cache_key, None)
+    qs = get_shouter_queryset()
+    text_list = [
+        f'{make_user_tag(s.user)} : <span class="text-dark">{strip_tags(s.text)}</span>'
+        for s in qs
+    ]
+    space = "&nbsp;" * 10
 
-        if cached_information:
-            return cached_information
+    informations = f"{space}".join(text_list)
 
-        qs = Shouter.objects.order_by("?")[:10]
+    if request.user.is_authenticated:
+        cache.set(cache_key, informations, 60 * 5)
 
-    text_list = []
-    for s in qs:
-        tag_user = make_user_tag(s.user)
-        text_list.append(tag_user + " : " + strip_tags(s.text))
-
-    informations = "   ".join(text_list)
-
-    cache.set(cache_key, informations, 60 * 5)
-
-    return informations
+    return mark_safe(informations)  # nosec
