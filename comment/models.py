32679ca9e3280944374db747dda66c735585140d
MatRos-sf <mateuszrosenkranz@gmail.com>
2024-04-24 10:56:49 +0200

    add Comment ABC model

--- comment/models.py
+++ comment/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Comment(models.Model):
+    """Abstract model represents comment section"""
+
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
+    comment = models.CharField(max_length=300)
+
+    created = models.DateTimeField(auto_now_add=True)
+    # TODO: status, report
+
+    class Meta:
+        abstract = True
+        ordering = ["-created"]
