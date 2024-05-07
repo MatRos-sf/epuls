6be80e466bf865f191f984799fe4d55adbb60833
MatRos-sf <mateuszrosenkranz@gmail.com>
2024-05-07 21:30:16 +0200

    create DiaryComment form and add this model to the admin panel

--- comment/admin.py
+++ comment/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import PhotoComment
+from .models import DiaryComment, PhotoComment
 
 admin.site.register(PhotoComment)
+admin.site.register(DiaryComment)
